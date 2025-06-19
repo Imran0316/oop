d20a6de563fc9307cb3f51a0766487e6c47e3afb
Imran <ik775239@gmail.com>
2025-06-19 15:05:35 +0500

    traits

--- traits.php
+++ traits.php
@@ -1,5 +1,40 @@
 <?php
 
-  
+trait message1{
+    public function msg1(){
+        echo "I AM LEARNING OOP" . "<br>";
+    }
+}
 
+class showmsg{
+    use message1;
+}
+
+$show1 = new showmsg();
+$show1->msg1();
+
+trait fruits{
+    public $name;
+    public function fruit_name($name){
+      $this->name = $name;
+    }
+}
+trait vegetables{
+    public $name;
+    public function veg_name($name){
+      $this->name = $name;
+    }
+}
+
+class items{
+    use fruits,vegetables;
+}
+
+$apple = new items();
+$apple->fruit_name("apple");
+echo $apple->name . "<br>";
+
+$onion = new items();
+$onion->veg_name("onion");
+echo $onion->name . "<br>";
 ?>
